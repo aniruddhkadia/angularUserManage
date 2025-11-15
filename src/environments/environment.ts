8ee92b60152ac914b36459f802b06c0308baa94c
aniruddh kadia <aniruddh.kadia@gmail.com>
2025-11-15 22:16:32 +0530

    environmant file changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5014/api'
+  //apiBaseUrl: 'http://localhost:5014/api'
+   apiBaseUrl: 'https://managementangular.runasp.net/api'
 };
