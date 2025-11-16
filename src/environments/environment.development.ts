fe542e836417b1cce7a0e756d81aca0ade8bf543
aniruddh kadia <aniruddh.kadia@gmail.com>
2025-11-16 17:37:18 +0530

    changes in env file

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,8 @@
 export const environment = {
+<<<<<<< Updated upstream
+=======
+  production: true,
+  //apiBaseUrl: 'http://localhost:5014/api'  
+>>>>>>> Stashed changes
   apiBaseUrl: 'https://managementangular.runasp.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,9 @@
 export const environment = {
+<<<<<<< Updated upstream
    apiBaseUrl: 'https://managementangular.runasp.net/api'
+=======
+  production: false,
+  //apiBaseUrl: 'http://localhost:5014/api'
+  apiBaseUrl: 'https://managementangular.runasp.net/api'
+>>>>>>> Stashed changes
 };
