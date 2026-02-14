37415d4c0184502b39978fdd20a1e97fd81cadf4
aniruddh kadia <aniruddh.kadia@gmail.com>
2026-02-14 17:16:18 +0530

    Improve auth guard formatting and redirect logic

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -3,15 +3,14 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
-const authService = inject(AuthService);
-const router = inject(Router)
-
-if(authService.isLoggedIn())
+  if (authService.isLoggedIn()) {
     return true;
-else {
-  router.navigateByUrl('/signin');
-    return false
+  } else {
+    // Redirect unauthorized access to login page
+    router.navigate(['/signin']);
+    return false;
   }
-
 };
