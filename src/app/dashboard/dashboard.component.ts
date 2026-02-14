79f1a4ff3f4e5513e9d9f285431fa6401d587bcd
aniruddh kadia <aniruddh.kadia@gmail.com>
2026-02-14 12:49:30 +0530

    Dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,30 +3,39 @@ import { Router } from '@angular/router';
 import { AuthService } from '../shared/services/auth.service';
 import { UserService } from '../shared/services/user.service';
 
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
-  styles: ``
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit {
-
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     private authService: AuthService,
-    private userService: UserService) { }
+    private userService: UserService,
+  ) {}
 
-    fullName: string = ''
+  fullName: string = '';
+  isSidebarVisible: boolean = false;
+
+  toggleSidebar() {
+    this.isSidebarVisible = !this.isSidebarVisible;
+  }
 
   ngOnInit(): void {
-     this.userService.getUserProfile().subscribe({
-      next: (res: any) => this.fullName = res.fullName,
-      error: (err: any) => console.log('error while retrieving user profile:\n', err)
-    })
+    this.userService.getUserProfile().subscribe({
+      next: (res: any) => (this.fullName = res.fullName),
+      error: (err: any) =>
+        console.log('error while retrieving user profile:\n', err),
+    });
   }
 
   onLogout() {
     this.authService.deleteToken();
     this.router.navigateByUrl('/signin');
   }
-
 }
